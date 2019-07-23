78002723720e7743cbbb5d5bad6d2261eef50956
Alex Zhukovich <zhukovich.a@gmail.com>
2019-07-23 10:42:13 +0200

    Added DatabaseConfig and DatabaseConnector classes

---
+++ v1/config/DatabaseConfig.php
@@ -0,0 +1,9 @@
+<?php
+    class DatabaseConfig {
+        const DB_HOST = 'localhost';
+        const DB_NAME = 'coffees_db';
+        const DB_CHARSET = 'utf8';
+        const DB_USERNAME = 'root';
+        const DB_PASSWORD = 'root';
+    }
+?>
