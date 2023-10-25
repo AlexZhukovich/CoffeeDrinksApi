a9badff4283578131f60ecfd6b76d7a1158c7888
Alex Zhukovich <zhukovich.a@gmail.com>
2023-10-25 18:13:38 +0200

    Added default ktor project

---
+++ coffee-drinks-api/src/main/kotlin/com/alexzh/coffeedrinks/api/Application.kt
@@ -0,0 +1,19 @@
+package com.alexzh.coffeedrinks.api
+
+import com.alexzh.coffeedrinks.api.plugins.configureHTTP
+import com.alexzh.coffeedrinks.api.plugins.configureRouting
+import com.alexzh.coffeedrinks.api.plugins.configureSecurity
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureSecurity()
+    configureHTTP()
+    configureRouting()
+}
