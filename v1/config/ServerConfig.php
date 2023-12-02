f9c3017239748bbceb98a11ba1272a7074c617aa
Alex Zhukovich <zhukovich.a@gmail.com>
2023-12-02 18:06:59 +0100

    Removed php and sql files

--- v1/config/ServerConfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-    class ServerConfig {
-        const HEADER_CONTENT_TYPE = 'Content-Type: application/json;charset=utf-8';
-        const HEADER_CACHE = 'Cache-control: max-age=60';
-        const HEADER_NO_CACHE = 'Cache-control: no-cache, no-store';
-
-        const ITEMS_ON_PAGE = 20;
-    }
-?>
